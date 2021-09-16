07a990c9543e81ddf172b437bfe9d3edc8738d09
ray pavlis <61029871+ray-pavlis-create@users.noreply.github.com>
2021-09-16 12:26:05 -0600

    Add files via upload

---
+++ food.py
@@ -0,0 +1,30 @@
+import random
+
+
+class Food:
+    block_size = None
+    x = 0
+    y = 0
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, g, b)
+    bounds = None
+
+    def __init__(self, block_size, bounds):
+        self.block_size = block_size
+        self.bounds = bounds
+
+    def draw(self, game, window):
+        game.draw.rect(window, self.color,
+                       (self.x, self.y, self.block_size, self.block_size))
+
+    def respawn(self):
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        self.color = (r, g, b)
+        blocks_in_x = (self.bounds[0]) / self.block_size
+        blocks_in_y = (self.bounds[1]) / self.block_size
+        self.x = random.randint(0, blocks_in_x - 1) * self.block_size
+        self.y = random.randint(0, blocks_in_y - 1) * self.block_size
