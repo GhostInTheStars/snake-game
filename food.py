f39b902955789d78a9d74077794be0f540760f61
GhostyStars <61029871+GhostInTheStars@users.noreply.github.com>
2024-06-27 15:00:27 -0600

    Add files via upload

--- food.py
+++ food.py
@@ -24,7 +24,7 @@ class Food:
         g = random.randint(0, 255)
         b = random.randint(0, 255)
         self.color = [r, g, b]
-        blocks_in_x = (self.bounds[0]) / self.block_size
-        blocks_in_y = (self.bounds[1]) / self.block_size
+        blocks_in_x = (self.bounds[0]) // self.block_size
+        blocks_in_y = (self.bounds[1]) // self.block_size
         self.x = random.randint(0, blocks_in_x - 1) * self.block_size
         self.y = random.randint(0, blocks_in_y - 1) * self.block_size
